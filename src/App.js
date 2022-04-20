218ba9e918a9cd9b3385e8791f342e2981b5944a
engibaryan <xachik.engibar@gmail.com>
2022-04-20 15:37:41 +0400

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import { act } from 'react-dom/test-utils';
 
 function App() {
+  console.log('App is rendering');
+  let [player1Counter, setPlayer1Counter] = useState(10);
+  let [player2Counter, setPlayer2Counter] = useState(10);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+          <div> Player1</div>
+          <div>{player1Counter}</div>
+          <button onClick = {() => {
+           setPlayer1Counter((actual) => actual+1);
+            }
+          }>
+              +
+          </button>
+      </div>
+      <hr />
+    <div>
+        <div>Player 2</div>
+        <div>{player2Counter}</div>
+        <button onClick = {() => {
+           setPlayer2Counter((actual) => actual+1);
+            }
+          }>
+              +
+          </button>
+    </div>
+    <hr />
+    <button onClick = {() => {
+      setPlayer1Counter((actual) => actual -1);
+      setPlayer2Counter((actual) => actual -1);
+    }}>-</button>
+
+    <button onClick ={() => {
+      setPlayer1Counter(10);
+      setPlayer2Counter(10);
+    }}>reset</button>
     </div>
   );
 }
